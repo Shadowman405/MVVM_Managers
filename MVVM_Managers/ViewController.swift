e27e58804cfceed018f9a84266988986759f01a2
Maxim Mitin <maximus405@gmail.com>
2022-01-21 21:40:25 +0300

    Initial Commit

---
+++ MVVM_Managers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Managers
+//
+//  Created by Maxim Mitin on 21.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
